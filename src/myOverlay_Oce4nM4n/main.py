0f3b1bbf8c21af5dcc2d7286aed88f98bd19aa21
Oce4nM4n <ayushmansihag@gmail.com>
2025-07-25 19:10:15 +0200

    the app is now runnable by typing myOverlay in command line or by python3 -m myoverlay_oce4nm4n

--- src/myOverlay_Oce4nM4n/main.py
+++ src/myOverlay_Oce4nM4n/main.py
@@ -1,9 +1,6 @@
-# myoverlay_oce4nm4n/__main__.py
+# myoverlay_oce4nm4n/main.py
 
 from .side_panel_app import SidePanelApp
 
 def main():
     SidePanelApp().run()
-
-if __name__ == "__main__":
-    main()
